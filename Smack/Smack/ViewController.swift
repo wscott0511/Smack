133fe3bd551d27940a5b01dbd080e143fa7fc7bb
Ryan Scott <williamscott0511@gmail.com>
2020-08-07 09:35:03 -0700

    Project started

---
+++ Smack/Smack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by William Scott on 8/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
