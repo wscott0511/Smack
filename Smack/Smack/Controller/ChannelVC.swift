39d720ea0bff6b3beeec67024f569f2ceb17fee9
Ryan Scott <williamscott0511@gmail.com>
2020-08-12 12:01:09 -0700

    Push to make something work maybe

---
+++ Smack/Smack/Controller/ChannelVC.swift
@@ -0,0 +1,31 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by William Scott on 8/12/20.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
